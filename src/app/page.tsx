2d67d1d3706f3d2e93d0567bcc271edf1e9786bc
Briofita09 <felipe.osorio.santos@hotmail.com>
2024-10-25 23:59:41 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
           const history = await axios.get(`http://localhost:5000/state/${ufsBrasil[selectedState].toLowerCase()}`);
           setData(history.data);
         } else {
-          setData([]); // Limpa os dados se nenhum estado estiver selecionado
+          setData([]); 
         }
       } catch (err) {
         console.log(err);
@@ -73,27 +73,27 @@ export default function Home() {
           <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
             {data && data.map((el, index) => {
               const state = Object.entries(el);
-              const citiesGroup = state[0][1]; // Presumindo que `citiesGroup` está na primeira entrada
+              const citiesGroup = state[0][1]; /
 
               let previousData = '';
-              const grupos = []; // Array para armazenar os grupos renderizáveis
+              const grupos = []; 
 
-              // Iterando sobre cada número e pegando a porcentagem de cada candidato
+
               Object.entries(citiesGroup).forEach(([key, value]) => {
                 const currentData = value.data;
 
-                // Verifica se a data atual é diferente da anterior
+              
                 if (currentData !== previousData) {
                   grupos.push(<h3 key={`data-${currentData}`}>Pesquisa {currentData}</h3>);
-                  previousData = currentData; // Atualiza a data anterior
+                  previousData = currentData; 
                 }
 
-                // Adiciona o grupo com candidatos
+
                 grupos.push(
                   <div key={`grupo-${key}`} style={{ border: '1px solid #ccc', padding: '10px', borderRadius: '5px', flex: '1 1 200px' }}>
                     <h4>Grupo {key}:</h4>
                     {Object.entries(value.candidatos).map(([candidato, detalhes]) => {
-                      // Formata o nome do candidato
+                     
                       const formattedCandidato = candidato.charAt(0).toUpperCase() + candidato.slice(1).replace(/([A-Z])/g, ' $1').trim();
                       return (
                         <p key={candidato}>{formattedCandidato}: {detalhes.porcentagem}%</p>
@@ -103,7 +103,7 @@ export default function Home() {
                 );
               });
 
-              return <div key={`state-${index}`} style={{ flex: '1 1 250px' }}>{grupos}</div>; // Agrupa todos os grupos para a cidade atual
+              return <div key={`state-${index}`} style={{ flex: '1 1 250px' }}>{grupos}</div>; 
             })}
           </div>
         </div>
